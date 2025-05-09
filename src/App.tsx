5a36943595e51658b3eff17983f0a884067cc2c3
Lip-ok <lipenjob@email.com>
2025-05-09 11:40:24 +0300

    feat: add button component

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from './components/button'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState<number>(0)
 
   return (
     <>
@@ -18,9 +19,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+
+        <Button setCount={setCount} count={count}/>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
